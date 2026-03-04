19e58b8600dcf6befbf917a5cfe94481c92881e3
Septieme7 <lordstiko7@gmail.com>
2026-03-04 16:13:38 +0100

    Minimisation des fichiers

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'courses-malin-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/css/style2.css',
+  '/assets/js/script.min.js',
+  '/assets/js/i18n.js',
+  '/assets/js/units.js',
+  '/assets/js/dragdrop.js',
+  '/assets/js/speech.js',
+  '/assets/js/geolocation.js',
+  '/assets/images/LogoCourses.webp',
+  '/assets/images/QRcode7.webp',
+  '/assets/images/iconeCSV.svg',
+  '/assets/images/iconeIMPORT.svg',
+  '/assets/images/iconeTXT.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
